f8815756e6344d7a3fd34e03f8645aca8317b8e4
cYpHeR <github.com/cypher-nullbyte>
2021-02-07 18:43:29 +0530

    Made A simple App including  almost all React Compnnet(s)  features

---
+++ src/hoc/Aux.js
@@ -0,0 +1,4 @@
+//ON windows Aux is reserved keyword. Hence use 'Auxilary.js' as fileName
+const Aux=props=>props.children;
+export default Aux;
+// We can use React.Fragment which do the same as Aux
